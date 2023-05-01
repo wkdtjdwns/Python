6e0bbf28379d159f470c8ecd3e5a64cc4830967e
Sung Jun Jang <128266768+wkdtjdwns@users.noreply.github.com>
2023-05-01 20:16:21 +0900

    Create 시험 성적.py

---
+++ self/baekjoon/Bronze V/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+S = int(input())
+if S >= 90 and S <= 100:
+    print('A')
+elif S >= 80 and S < 90:
+    print('B')
+elif S >= 70 and S < 80:
+    print('C')
+elif S >= 60 and S < 70:
+    print('D')
+else:
+    print('F')
