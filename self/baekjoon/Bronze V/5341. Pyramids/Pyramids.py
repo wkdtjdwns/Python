26fb2f45cdfc077fdae4911b26e0dcaeb12f67e2
Sung Jun Jang <128266768+wkdtjdwns@users.noreply.github.com>
2023-05-05 13:54:11 +0900

    Update Pyramids.py

--- self/baekjoon/Bronze V/5341. Pyramids/Pyramids.py
+++ self/baekjoon/Bronze V/5341. Pyramids/Pyramids.py
@@ -1,8 +1,8 @@
 while True:
-    block = 0
+    blocks = 0
     n = int(input())
     for i in range(n, 0, -1):
-        block += i
+        blocks += i
     if n == 0:
         break
-    print(block)
+    print(blocks)
