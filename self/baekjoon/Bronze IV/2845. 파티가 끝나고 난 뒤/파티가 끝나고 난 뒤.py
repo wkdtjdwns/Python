8f2c2857f723a8310f2f62518e4d5dcdc24cde47
Sung Jun Jang <128266768+wkdtjdwns@users.noreply.github.com>
2023-05-08 22:54:06 +0900

    Create 파티가 끝나고 난 뒤.py

---
+++ self/baekjoon/Bronze IV/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.py
@@ -0,0 +1,3 @@
+people, area = map(int, input().split())
+a, b, c, d, e = map(int, input().split())
+print((a - people*area), (b - people*area), (c - people*area), (d - people*area), (e - people*area))
