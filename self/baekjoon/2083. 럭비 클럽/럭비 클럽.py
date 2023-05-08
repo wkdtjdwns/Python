8424d386ba99ade2ca2b4826f785c2356774b400
Sung Jun Jang <128266768+wkdtjdwns@users.noreply.github.com>
2023-05-08 21:27:42 +0900

    Create 럭비 클럽.py

---
+++ self/baekjoon/2083. 럭비 클럽/럭비 클럽.py
@@ -0,0 +1,8 @@
+while True:
+    name, age, weight = map(str, input().split())
+    if name == "#" and age == "0" and weight == "0":
+        break
+    elif int(age) > 17 or int(weight) >= 80:
+        print(name + " Senior")
+    else:
+        print(name + " Junior")
