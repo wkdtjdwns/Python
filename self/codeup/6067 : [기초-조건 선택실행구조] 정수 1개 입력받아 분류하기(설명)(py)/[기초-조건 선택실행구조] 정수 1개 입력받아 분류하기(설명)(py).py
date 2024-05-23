abf7bb2ba7c257ca1361d77643a6b48a24e09e27
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-05-23 19:16:57 +0900

    Create [기초-조건 선택실행구조] 정수 1개 입력받아 분류하기(설명)(py).py

---
+++ self/codeup/6067 : [기초-조건 선택실행구조] 정수 1개 입력받아 분류하기(설명)(py)/[기초-조건 선택실행구조] 정수 1개 입력받아 분류하기(설명)(py).py
@@ -0,0 +1,15 @@
+n = int(input())
+if (n < 0) and (n % 2 == 0):
+    print('A')
+
+elif (n < 0) and (n % 2 != 0):
+    print('B')
+
+elif (n < 0) and (n % 2 != 0):
+    print('B')
+
+elif (n > 0) and (n % 2 == 0):
+    print('C')
+
+else:
+    print('D')
