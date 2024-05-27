1cbdc6e4fc392b43bd1755c8b4383d1392c84e61
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-05-27 10:55:14 +0900

    Update [기초-종합] 그림 파일 저장용량 계산하기(py).py

--- self/codeup/6085 : [기초-종합] 그림 파일 저장용량 계산하기(py)/[기초-종합] 그림 파일 저장용량 계산하기(py).py
+++ self/codeup/6085 : [기초-종합] 그림 파일 저장용량 계산하기(py)/[기초-종합] 그림 파일 저장용량 계산하기(py).py
@@ -1,2 +1,4 @@
-w, h, b = map(int, input().split(' '))
-print(round( (w * h * b / 8 / 1024 / 1024), 2), "MB")
+w,h,b = input().split()
+result = int(w) * int(h) * int(b) / 1024 / 1024 /8
+
+print('%.2f'%result,"MB")
