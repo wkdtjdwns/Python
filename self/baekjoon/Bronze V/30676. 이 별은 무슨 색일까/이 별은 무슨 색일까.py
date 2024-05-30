7e286accaa4e57fe7a1292842642f89c2390ad2e
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-05-30 11:09:15 +0900

    Create 이 별은 무슨 색일까.py

---
+++ self/baekjoon/Bronze V/30676. 이 별은 무슨 색일까/이 별은 무슨 색일까.py
@@ -0,0 +1,20 @@
+lamb = int(input())
+
+if lamb >= 620:
+    print('Red')
+elif lamb >= 590:
+    print('Orange');
+elif lamb >= 590:
+    print('Orange');
+elif lamb >= 570:
+    print('Yellow');
+elif lamb >= 495:
+    print('Green');
+elif lamb >= 590:
+    print('Orange');
+elif lamb >= 450:
+    print('Blue');
+elif lamb >= 425:
+    print('Indigo');
+else:
+    print('Violet');
