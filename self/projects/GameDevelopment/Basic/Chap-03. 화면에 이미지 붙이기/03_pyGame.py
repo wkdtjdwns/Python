fb78f43c129db952e4d4d4c5a0e5ccda48f61535
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-06-11 20:35:39 +0900

    Create 03_pyGame.py

---
+++ self/projects/GameDevelopment/Basic/Chap-03. 화면에 이미지 붙이기/03_pyGame.py
@@ -0,0 +1,38 @@
+#---------------------------------게임 개발의 기본 설정----------------------------------
+import pygame
+
+import pygame.docs # Python을 활용한 게임 개발을 위한 라이브러리
+
+# pygame을 사용하기 위한 초기화
+pygame.init()
+
+#---------------------------------게임 창에 대한 설정----------------------------------
+# 게임 창의 크기 설정
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("이미지 붙이기")
+
+#---------------------------------이미지 붙이기 및 설정----------------------------------
+snake_image = pygame.image.load("snake.png")
+snake_rect = snake_image.get_rect() # snake_image의 영역을 가져오는 함수
+snake_rect.topleft = (100, 100)
+
+#---------------------------------게임 실행 코드----------------------------------
+running = True
+while running:
+    # 존재하는 모든 event들을 event 변수에 넣고
+    for event in pygame.event.get():
+        # event 변수의 값이 QUIT(나가기)라면
+        if event.type == pygame.QUIT:
+            # 실행을 멈춤
+            running = False
+
+    display_surface.blit(snake_image, snake_rect) # 이미지를 화면에 붙이는 함수
+
+    # 디스플레이 업데이트 (이게 없으면 위에 있는 코드들이 게임에 업데이트 되지 않아서 적용되지 않음)
+    pygame.display.update()
+
+# 게임을 나감
+pygame.quit()
