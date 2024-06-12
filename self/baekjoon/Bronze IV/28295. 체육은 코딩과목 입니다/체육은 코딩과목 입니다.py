55d7712bedc261fd00e38d8f3ffe92aa0265310a
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-06-12 15:34:07 +0900

    Create 체육은 코딩과목 입니다.py

---
+++ self/baekjoon/Bronze IV/28295. 체육은 코딩과목 입니다/체육은 코딩과목 입니다.py
@@ -0,0 +1,11 @@
+direction = ['N', 'E', 'S', 'W']
+result = 'N'
+
+for i in range(10):
+    t = int(input())
+    
+    if t == 1: result = direction[(direction.index(result) + 1) % 4]
+    elif t == 2: result = direction[(direction.index(result) + 2) % 4]
+    elif t == 3: result = direction[(direction.index(result) + 3) % 4]
+
+print(result)
