7f08cd9d7221ad7cd06ec1e57687719b764b8864
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-06-15 13:53:55 +0900

    [2024-06-15]

---
+++ self/baekjoon/Bronze IV/30402. 감마선을 맞은 컴퓨터/감마선을 맞은 컴퓨터.py
@@ -0,0 +1,15 @@
+board = []
+for i in range(15):
+    board = list(map(str, input().split(' ')))
+
+    if 'w' in board:
+        print('chunbae')
+        break
+
+    elif 'b' in board:
+        print('nabi')
+        break
+
+    elif 'g' in board:
+        print('yeongcheol')
+        break
