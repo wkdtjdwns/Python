1b6cb94e65c37aea28d521dcac42fa21781e5176
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-06-30 18:43:57 +0900

    [2024-06-30]

---
+++ self/baekjoon/Bronze III/3029. 경고/경고.py
@@ -0,0 +1,12 @@
+h1, m1, s1 = map(int, input().split(':'))
+h2, m2, s2 = map(int, input().split(':'))
+
+t1 = h1 * 3600 + m1 * 60 + s1
+t2 = h2 * 3600 + m2 * 60 + s2
+
+t = t2 - t1 if t2 > t1 else (t2 - t1) + 24 * 3600
+h = t // 3600
+m = (t // 60) % 60
+s = t % 60
+
+print("%02d:%02d:%02d" % (h, m, s))
