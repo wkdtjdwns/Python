1b2069cfa468e521a414c19411249aac60ccc1f1
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-03 15:04:38 +0900

    [2024-07-03]

---
+++ self/baekjoon/Bronze III/4504. 배수 찾기/배수 찾기.py
@@ -0,0 +1,6 @@
+n = int(input())
+while True:
+    num = int(input())
+    if num == 0: break
+    if num % n == 0: print(f'{num} is a multiple of {n}')
+    else: print(f'{num} is NOT a multiple of {n}')
