d569e6ae1fc89a38a86c370566bd6ac25c4508c7
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-06 15:27:16 +0900

    [2024-07-06]

--- self/baekjoon/Bronze III/5597. 과제 안 내신 분..?/과제 안 내신 분...?.py
+++ self/baekjoon/Bronze III/5597. 과제 안 내신 분..?/과제 안 내신 분...?.py
@@ -1,10 +1,7 @@
-# 학생들의 번호를 리스트에 저장 후
 std = [i for i in range(1,31)] 
 for _ in range(28):
-  # 학생들의 번호를 입력받고
   a = int(input())    
-  # 입력받은 숫자를 리스트에서 지운 뒤
   std.remove(a)
-# 작은 번호부터 출력
+  
 print(min(std))
 print(max(std))
