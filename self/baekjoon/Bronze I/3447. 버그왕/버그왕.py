ce7eebf70f5caa0c0a1b263e3b6b7e8dfeedc160
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-22 21:44:12 +0900

    [2024-07-22]

---
+++ self/baekjoon/Bronze I/3447. 버그왕/버그왕.py
@@ -0,0 +1,13 @@
+import sys 
+import re
+
+code = sys.stdin.readlines()
+for i in code:     
+    while True:
+        result = re.sub('BUG', '', i)
+        if 'BUG' in result:
+            i = result
+
+        else: 
+            print(result, end='')
+            break
