6f4c83bddcd1cbaadc7e56378c31ac1b5043f3df
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-27 17:21:34 +0900

    [2024-07-27]

---
+++ self/baekjoon/Bronze III/16431. 베시와 데이지/베시와 데이지.py
@@ -0,0 +1,9 @@
+br, bc = map(int, input().split(' '))
+dr, dc = map(int, input().split(' '))
+jr, jc = map(int, input().split(' '))
+b = max(abs(jr - br), abs(jc - bc))
+d = abs(jr - dr) + abs(jc - dc)
+
+if b > d: print("daisy")
+elif b < d: print("bessie")
+else: print("tie")
