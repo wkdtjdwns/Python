09a6ac98e85edd3982b82af7ced281ca63d2e886
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-08-06 17:07:56 +0900

    [2024-08-06]

---
+++ self/baekjoon/Bronze III/23812. 골뱅이 찍기 - 돌아간 ㅍ/골뱅이 찍기 - 돌아간 ㅍ.py
@@ -0,0 +1,21 @@
+n = int(input())
+for _ in range(n):
+    print('@' * n, end='')
+    print(' ' * (n * 3), end='')
+    print('@' * n)
+
+for _ in range(n):
+    print('@' * (n * 5))
+
+for _ in range(n):
+    print('@' * n, end='')
+    print(' ' * (n * 3), end='')
+    print('@' * n)
+
+for _ in range(n):
+    print('@' * (n * 5))
+
+for _ in range(n):
+    print('@' * n, end='')
+    print(' ' * (n * 3), end='')
+    print('@' * n)
