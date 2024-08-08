7c901be34938f47594673d38fccb8a0b25fcb500
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-08-08 20:13:32 +0900

    [2024-08-08]

---
+++ self/baekjoon/Bronze III/25024. 시간과 날짜/시간과 날짜.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+a1 = [1, 3, 5, 7, 8, 10, 12]
+a2 = [4, 6, 9, 11]
+for _ in range(int(input())):
+    A = "No"
+    B = "No"
+    a, b, = map(int, input().split(' '))
+    if 0 < a <= 12:
+        if a in a1 and 0 < b <= 31:
+            A = "Yes"
+
+        elif a in a2 and 0 < b <= 30:
+            A = "Yes"
+        elif a == 2 and 0 < b <= 29:
+            A = "Yes"
+
+    if 0 <= a <= 23:
+        if b <= 59:
+            B = "Yes"
+
+    print(B, A)
